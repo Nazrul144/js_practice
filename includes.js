6b59b0fcb93e9a3b53f249c6c42c3327802cd50c
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-04-29 18:00:17 +0600

    includes practice

---
+++ includes.js
@@ -0,0 +1,5 @@
+const universities = ['Dhaka University', 'Green University', 'National University', 'Uttara University', 'Independent University', 'Defodil University'];
+
+const result = universities.includes('National University')
+
+console.log(result);
