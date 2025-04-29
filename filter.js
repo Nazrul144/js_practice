5e7db1c369af09fa5e70ebcac608cf872c62b39e
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-04-29 17:47:03 +0600

    Filter practice

---
+++ filter.js
@@ -0,0 +1,6 @@
+const name = ['Nazrul', 'Rasel', 'Fahad', 'Rakib', 'Raju', 'Ismail', 'Shahidul'];
+const targetName = ['Raju', 'Nazrul', 'Rakib']
+
+const filterName = name.filter(e => e === 'Fahad' );
+
+console.log(filterName);
