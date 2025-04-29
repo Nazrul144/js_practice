dd7d2f2198dfdec03c109d8965c3398f728c62f6
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-04-29 17:49:39 +0600

    Filter practice

--- filter.js
+++ filter.js
@@ -3,4 +3,10 @@ const targetName = ['Raju', 'Nazrul', 'Rakib']
 
 const filterName = name.filter(e => e === 'Fahad' );
 
-console.log(filterName);
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const filterNumbers = numbers.filter((n=> n<=3))
+
+console.log(filterNumbers);
